ea2a8bb1e927a476f5637b8c6d6f1a1f8e4d2c86
Praneeth <madasu.praneeth464@gmail.com>
2025-06-02 22:52:14 +0530

    Footer done

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,7 +1,14 @@
+import Content from "./Content";
+
 const Footer = () => {
   return (
-    <div className="relative h-[800px]">
-      <div className="relative h-[calc(100vh+800px)]"></div>
+    <div
+      className="relative h-[800px]"
+      style={{ clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)" }}
+    >
+      <div className="fixed bottom-0 h-[800px] w-full">
+        <Content />
+      </div>
     </div>
   );
 };
--- src/components/ui/MainPage.tsx
+++ src/components/ui/MainPage.tsx
@@ -69,6 +69,7 @@ const MainPage = () => {
           <Faq />
         </div>
         {/*Footer */}
+        <Footer />
       </motion.div>
     </>
   );
